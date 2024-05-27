1ef359731d06a42d31f560ca0b9c9a4c5e1d4902
neptunejing <jingnanqing@hotmail.com>
2024-05-27 16:20:28 +0300

    ex9.5

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -2,31 +2,13 @@ export const calculateBmi = (heightCm: number, weightKg: number): string => {
     const heightM = heightCm / 100;
     const bmi = weightKg / (heightM * heightM);
 
-    let message;
     if (bmi < 18.5) {
-        message = "Underweight";
+        return "Underweight";
     } else if (bmi >= 18.5 && bmi < 24.9) {
-        message = "Normal (healthy weight)";
+        return "Normal (healthy weight)";
     } else if (bmi >= 25 && bmi < 29.9) {
-        message = "Overweight";
+        return "Overweight";
     } else {
-        message = "Obese";
+        return "Obese";
     }
-
-    return message;
 }
-
-
-const args = process.argv.slice(2);
-if (args.length !== 2) {
-    process.exit(1);
-}
-
-const [height, weight] = args.map(Number);
-
-if (isNaN(height) || isNaN(weight)) {
-    console.log('Arguments should be numbers');
-    process.exit(1);
-}
-
-console.log(calculateBmi(height, weight));
--- index.ts
+++ index.ts
@@ -1,9 +1,28 @@
 import express from 'express';
+import { calculateBmi } from './bmiCalculator';
 
 const app = express();
 
 app.get('/hello', (_req, res) => {
     res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+    const { height, weight } = req.query;
+
+    if (!height || !weight || isNaN(Number(height)) || isNaN(Number(weight))) {
+        return res.status(400).json({ error: 'malformatted parameters' });
+    }
+
+    const heightCm = Number(height);
+    const weightKg = Number(weight);
+    const bmi = calculateBmi(heightCm, weightKg);
+
+    return res.json({
+        weight: weightKg,
+        height: heightCm,
+        bmi
+    });
 });
 
 const PORT = 3003;
